96fa72d06209f8ccb4c502b14bd40371bc7f5dd4
alexis <alexisrob@netc.fr>
2024-10-02 09:25:57 +0200

    Création du getMapping pour consulter une actualité via son id

--- src/main/java/bts/sio/api/controller/ActualiteController.java
+++ src/main/java/bts/sio/api/controller/ActualiteController.java
@@ -5,6 +5,8 @@ import bts.sio.api.service.ActualiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 
 @RestController
 public class ActualiteController {
@@ -13,12 +15,27 @@ public class ActualiteController {
     private ActualiteService actualiteService;
 
     /**
-     * Read - Get all athletes
-     * @return - An Iterable object of Athlete full filled
+     * Read - Get all actualites
+     * @return - An Iterable object of actualite full filled
      */
     @GetMapping("/actualites")
     public Iterable<Actualite> getActualites() {
         return actualiteService.getActualites();
+    }
+
+    /**
+     * Read - Get one actualite
+     * @param id The id of the actualite
+     * @return An actualite object full filled
+     */
+    @GetMapping("/actualite/{id}")
+    public Actualite getActualite(@PathVariable("id") final Long id) {
+        Optional<Actualite> actualite = actualiteService.getActualite(id);
+        if(actualite.isPresent()) {
+            return actualite.get();
+        } else {
+            return null;
+        }
     }
 
 }
